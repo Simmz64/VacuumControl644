6df9e4ec2106edc1964b84277b9d96513a8618a5
Simmz64 <sim.gollbo@gmail.com>
2017-03-16 16:21:34 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * VacuumControl644.c
+ *
+ * Created: 2017-03-16 16:11:10
+ * Author : Simon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
